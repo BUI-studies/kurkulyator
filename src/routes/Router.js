f22999bf16e6b6f1ba406d6bebc9bb465be5b7db
Roman Ohiienko <ohiienko.r@gmail.com>
2023-10-01 12:37:13 +0300

    Updates to create pull request

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,28 +1,28 @@
-import { Login, Home } from "@/pages"
+import { Login, Home } from "@/pages";
 
-import { ROUTES_NAMES } from "./Router.helper.js"
+import { ROUTES_NAMES } from "./Router.helper.js";
 
-let pages = createPages()
+let pages = createPages();
 
-let currentRoute = null
-let currentPage = null
+let currentRoute = null;
+let currentPage = null;
 
-let currentUser = null
-export const getCurrentUser = () => currentUser
-export const setCurrentUser = (u) => (currentUser = u)
-
+let currentUser = null;
+export const getCurrentUser = () => currentUser;
+export const setCurrentUser = (u) => (currentUser = u);
+//TODO: check if router works
 export const navigate = (route) => {
   if (!ROUTES_NAMES.values.includes(route))
-    throw new Error(`There is no such route: ${route}!`)
+    throw new Error(`There is no such route: ${route}!`);
 
-  currentRoute = ROUTES[route] //keeps in memory the url part responsible for the page
-  history.pushState({}, "", `${currentRoute}`)
+  currentRoute = ROUTES[route]; //keeps in memory the url part responsible for the page
+  history.pushState({}, "", `${currentRoute}`);
 
-  currentPage = pages[route]
+  currentPage = pages[route];
 
-  currentPage.render()
-}
+  currentPage.render();
+};
 
 export const clearRoutes = () => {
-  pages = createPages()
-}
+  pages = createPages();
+};
