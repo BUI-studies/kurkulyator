9642ff6322f20e323688761cd973d8ea1ec4a2be
IvanShablii <ivan182sh@gmail.com>
2023-10-04 10:36:53 +0300

    update header and finish task

--- src/template/index.js
+++ src/template/index.js
@@ -1,3 +1,9 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
 export default function Page() {}
 
-Page.prototype.render = function () {}
+Page.prototype.render = function () {
+  this.footer = new Footer();
+  this.header = new Header();
+};
