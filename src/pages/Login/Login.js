b1042784c83f7ee465877f0bda136143e27898a4
Nick Yamnyk <nick.yamnyk@nedap.com>
2023-10-08 10:56:36 +0300

    fix broken merge

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,27 +1,25 @@
-
-import { googleAuthProvider, auth } from "../../../firebase";
-import { signInWithPopup } from "firebase/auth";
+import { googleAuthProvider, auth } from "../../../firebase"
+import { signInWithPopup } from "firebase/auth"
 
 export default function Login() {
-  this.header = document.createElement("h2");
-  this.googleButton = document.createElement("button");
-  this.pageWrapper = document.createElement("div");
+  this.header = document.createElement("h2")
+  this.googleButton = document.createElement("button")
+  this.pageWrapper = document.createElement("div")
 }
 
-Login.prototype.render = (parent) => {
-  this.parent = parent;
-  this.header.textContent = "Please log in with:";
-  this.googleButton.textContent = "Google";
+Login.prototype.render = function (parent) {
+  this.parent = parent
+  this.header.textContent = "Please log in with:"
+  this.googleButton.textContent = "Google"
   this.googleButton.addEventListener("click", (e) =>
     this.handleSignInButtonClick(e)
-  );
+  )
 
-  this.pageWrapper.append(this.header, this.googleButton);
-  this.parent.append(this.pageWrapper);
-};
+  this.pageWrapper.append(this.header, this.googleButton)
+  this.parent.append(this.pageWrapper)
+}
 
-Login.prototype.handleSignInButtonClick = (e) => {
-  e.preventDefault();
-  signInWithPopup(auth, googleAuthProvider);
-};
-
+Login.prototype.handleSignInButtonClick = function (e) {
+  e.preventDefault()
+  signInWithPopup(auth, googleAuthProvider)
+}
