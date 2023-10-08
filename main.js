eec2e3aab573708df673ab2b3ecdd1a8c4765afe
AKot1988 <kulibabenko@ukr.net>
2023-10-08 11:42:45 +0300

    delete console.log

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase';
 import { Router, ROUTES_NAMES } from '@/routes';
-import { getTransactions } from './src/API';
 
-getTransactions().then((res) => console.log(res));
 
 onAuthStateChanged(auth, (user) => {
   console.log(user);
