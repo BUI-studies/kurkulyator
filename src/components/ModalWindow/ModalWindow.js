8aab1fd01bc9145b47322daf4f1fdecb6af790f8
IvanShablii <ivan182sh@gmail.com>
2023-10-25 10:59:49 +0300

    add modal window

---
+++ src/components/ModalWindow/ModalWindow.js
@@ -0,0 +1,27 @@
+import "./ModalWindow.scss";
+
+export default function ModalWindow() {
+  this.modal = document.createElement("div");
+  this.content = document.createElement("div");
+}
+
+ModalWindow.prototype.render = function (parent, content) {
+  this.modal.classList.add("modal");
+  this.content.classList.add("modal__content");
+
+  content.render(this.content);
+
+  this.modal.append(this.content);
+
+  this.modal.addEventListener("click", (event) => {
+    if (event.target === this.modal) {
+      this.close();
+    }
+  });
+
+  parent.append(this.modal);
+};
+
+ModalWindow.prototype.close = function () {
+  this.modal.remove();
+};
