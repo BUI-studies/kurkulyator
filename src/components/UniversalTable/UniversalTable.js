dee803fc04d64ebe0e0fc4c975c54e4930446de8
IvanShablii <ivan182sh@gmail.com>
2023-10-25 10:14:43 +0300

    add transactions to home page

--- src/components/UniversalTable/UniversalTable.js
+++ src/components/UniversalTable/UniversalTable.js
@@ -4,18 +4,28 @@ export default function UniversalTable(collection, options) {
   this.rowClick = options.onClick;
   this.collection = collection;
   this.headers = options.headers;
+  this.emptyCellValue = options.emptyCellValue || "";
+  this.classes = {
+    cell: options?.classes?.cell || "table-cell",
+    row: options?.classes?.row || "table-row",
+    table: options?.classes?.table || "table-body",
+  };
 }
 
 UniversalTable.prototype.render = function (parent) {
   const tableHeader = document.createElement("ul");
+  tableHeader.classList.add(this.classes.table);
+
   const tableHeaderRow = document.createElement("li");
+  tableHeaderRow.classList.add(this.classes.row);
 
   this.headers.forEach((cell) => {
-    tableHeaderRow.innerHTML += `<span>${cell.title}</span>`;
+    tableHeaderRow.innerHTML += `<span class="${this.classes.cell}">${cell.title}</span>`;
   });
   tableHeader.append(tableHeaderRow);
 
   const tableBody = document.createElement("ul");
+  tableBody.classList.add(this.classes.table);
 
   if (!this.rowClick) {
     console.log("No row click callback attached");
@@ -27,11 +37,18 @@ UniversalTable.prototype.render = function (parent) {
 
   this.collection.forEach((row, index) => {
     const tableRow = document.createElement("li");
+    tableRow.classList.add(this.classes.row);
+
     tableRow.dataset.ind = index;
     tableRow.classList.add("t-row");
 
     tableRow.innerHTML = this.headers
-      .map(({ name }) => `<span>${row[name]}</span>`)
+      .map(
+        ({ name }) =>
+          `<span class="${this.classes.cell}">${
+            row[name] || this.emptyCellValue
+          }</span>`
+      )
       .join("");
 
     tableBody.append(tableRow);
@@ -43,8 +60,8 @@ UniversalTable.prototype.render = function (parent) {
 UniversalTable.prototype.rowClickHandler = function (e) {
   let clickedIndex = 0;
 
-  if (!e.target.classList.contains("t-row")) {
-    clickedIndex = e.target.closest(".t-row").dataset;
+  if (!e.target.classList.contains(this.classes.row)) {
+    clickedIndex = e.target.closest(`.${this.classes.row}`).dataset;
   } else {
     clickedIndex = e.target.dataset;
   }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
-import { getWallets } from "@/API";
+import { getDoc } from "firebase/firestore";
+
+import { getWallets, getTransactions } from "@/API";
+import { UniversalTable } from "@/components";
+
 import "./Home.scss";
 
 export default function Home() {
   this.pageWrapper = document.createElement("div");
-  this.placeholderText = document.createElement("h2"); // temp placeholder
-  this.placeholderText.textContent = "Home page";
   this.balanceWrapper = document.createElement("section");
   this.balanceText = document.createElement("h2");
   this.totalBalance = document.createElement("h2");
@@ -12,6 +14,10 @@ export default function Home() {
 
   this.walletsWrapper = document.createElement("section");
   this.walletsHeader = document.createElement("h2");
+
+  this.transactionsWrapper = document.createElement("section");
+  this.transactionsHeader = document.createElement("h2");
+  this.transactionsTable = null;
 }
 
 Home.prototype.render = async function (parent) {
@@ -26,24 +32,65 @@ Home.prototype.render = async function (parent) {
   this.walletsWrapper.classList.add("wallets--wrapper");
   this.walletsHeader.textContent = "Your wallets";
 
-  let wallets = await getWallets();
-  let totalBalance = 1120.54;
-  wallets.forEach((item) => (totalBalance += +item.balance));
+  this.transactionsWrapper.classList.add("transactions--wrapper");
+  this.transactionsHeader.classList.add("transactions--header");
+  this.transactionsHeader.textContent = "Transactions";
+
+  const wallets = await getWallets();
+
+  const totalBalance = wallets.reduce(
+    (acc, currWallet) => (acc += +currWallet.balance),
+    0
+  );
   this.totalBalance.textContent = totalBalance;
 
-  console.log(wallets);
+  const transactionsWithRefs = await getTransactions();
+  const transactions = await Promise.all(
+    transactionsWithRefs.map(async (t) => ({
+      ...t,
+      to: t.to ? (await getDoc(t.to)).data().name : null,
+      from: t.from ? (await getDoc(t.from)).data().name : null,
+      category: (await getDoc(t.category)).data().name,
+      date: t.date.toDate().toLocaleString(),
+      comment: !t.comment ? "Empty" : t.comment,
+    }))
+  );
+
+  this.transactionsTable = new UniversalTable(transactions, {
+    headers: [
+      { name: "category", title: "Category" },
+      { name: "amount", title: "Amount" },
+      { name: "from", title: "From" },
+      { name: "to", title: "To" },
+      { name: "comment", title: "Comment" },
+      { name: "date", title: "Date" },
+      { name: "type", title: "Type" },
+    ],
+  });
+
+  this.transactionsWrapper.replaceChildren();
+  this.transactionsWrapper.append(this.transactionsHeader);
+  this.transactionsTable.render(this.transactionsWrapper);
+
+  this.walletsTable = new UniversalTable(wallets, {
+    headers: [
+      { name: "name", title: "Name" },
+      { name: "balance", title: "Balance" },
+    ],
+  });
 
   this.balanceWrapper.append(
     this.balanceText,
     this.currency,
     this.totalBalance
   );
-
+  this.walletsWrapper.replaceChildren();
   this.walletsWrapper.append(this.walletsHeader);
+  this.walletsTable.render(this.walletsWrapper);
   this.pageWrapper.append(
-    this.placeholderText,
     this.balanceWrapper,
-    this.walletsWrapper
+    this.walletsWrapper,
+    this.transactionsWrapper
   );
   parent.append(this.pageWrapper);
 };
