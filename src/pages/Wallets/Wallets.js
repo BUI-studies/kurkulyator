0a160a5a1960b9d99eb2645277676d65488f53f9
olegtymo <oleg_tymo@ukr.net>
2023-10-25 11:16:38 +0300

    has updated wallets

--- src/pages/Wallets/Wallets.js
+++ src/pages/Wallets/Wallets.js
@@ -1,17 +1,25 @@
-import { getWallets } from "@/API";
+import { getWallets } from '@/API';
+import { UniversalTable } from '@/components';
 
 export default function Wallets() {
-  this.pageWrapper = document.createElement("div");
-  this.addButton = document.createElement("button");
+  this.pageWrapper = document.createElement('div');
+  this.addButton = document.createElement('button');
 
-  this.placeholderText = document.createElement("h2"); // temp placeholder
-  this.placeholderText.textContent = "Wallets page";
+  this.placeholderText = document.createElement('h2'); // temp placeholder
+  this.placeholderText.textContent = 'Wallets page';
 }
 
 Wallets.prototype.render = async function (parent) {
   this.wallets = await getWallets();
 
-  this.addButton.textContent = "New wallet";
+  this.walletTable = new UniversalTable(wallets, {
+    headers: [
+      { name: 'title', title: 'Title' },
+      { name: 'balance', title: 'Balance' },
+    ],
+  });
+
+  this.addButton.textContent = 'New wallet';
   this.addButton.onclick = (e) => this.handleNewWalletClick(e);
   this.pageWrapper.append(this.placeholderText, this.addButton);
 
