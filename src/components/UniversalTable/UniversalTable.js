afad947c94fd409822b3e26025347ba66b866be3
olegtymo <oleg_tymo@ukr.net>
2023-12-03 10:44:37 +0200

    added unsorted and sorted classes

--- src/components/UniversalTable/UniversalTable.js
+++ src/components/UniversalTable/UniversalTable.js
@@ -1,79 +1,157 @@
-import "./UniversalTable.scss";
+import './UniversalTable.scss';
 
+const ARROW_DOWN = '&#9660';
+const ARROW_UP = '&#9650';
+
+/**
+ * @type Classes
+ *
+ * @property {string} cell - class name for the row. By default it is - 'table-cell'.
+ * @property {string} celUnsorted - class name for the unsorted row. By default it is - 'table-cell--unsorted'.
+ * @property {string} celSorted - class name for the sorted row. By default it is - 'table-cell--sorted'.
+ * @property {string} row - class name for the row. By default it is - 'table-row'.
+ * @property {string} table - class name for the row. By default it is - 'table-body'.
+ */
+
+/**
+ * @type Header
+ *
+ * @property {string} name - the name of the object's property. Gonna be used as a variable to extract the value from the object like so - collectionItem[headerName]
+ * @property {string} title - how you would like to show the property on the screen
+ * @property {Boolean | undefined} sortBy - an indicator to sort the collection by this field. Should be only one per collection, if passed more than one - only the first will be used.
+ */
+
+/**
+ * @type Options
+ *
+ * @property {Header[]} headers - array of table column headers that should be shown as a first row
+ * @property {any} emptyCellValue - literally any value you'd like to put to represent emptiness
+ * @property {Classes} classes - classes to style up the table. If some of the class names are not in your object - they will be replaced by the default class names.
+ */
+
+/**
+ * @constructor UniversalTable
+ *
+ * @param {object[]} collection - collection to be shown as a table on the screen
+ * @param {Options} config - configuration options for the UniversalTable
+ */
 export default function UniversalTable(collection, options) {
   this.rowClick = options.onClick;
-  this.collection = collection;
+
+  this.sortingHeader = options.headers.find(({ sortBy, sort }) => sortBy && sort);
+  console.log(this.sortingHeader);
+
+  if (!this.sortingHeader) throw new TypeError('No header found matching sorting header criteria!');
+
+  this.collection = collection.sort(this.sortingHeader.sort);
+
   this.headers = options.headers;
-  this.emptyCellValue = options.emptyCellValue || "";
+  this.emptyCellValue = options.emptyCellValue || '';
+  this.tableBody = document.createElement('ul');
   this.classes = {
-    cell: options?.classes?.cell || "table-cell",
-    row: options?.classes?.row || "table-row",
-    table: options?.classes?.table || "table-body",
+    cell: options?.classes?.cell || 'table-cell',
+    cellUnsorted: options?.classes?.cellUnsorted || 'table-cell--unsorted',
+    cellSorted: options?.classes?.cellSorted || 'table-cell--sorted',
+    row: options?.classes?.row || 'table-row',
+    table: options?.classes?.table || 'table-body',
   };
-  this.generateDataset = options.generateDataset;
 }
 
 UniversalTable.prototype.render = function (parent) {
-  const tableHeader = document.createElement("ul");
+  const tableHeader = document.createElement('ul');
   tableHeader.classList.add(this.classes.table);
 
-  const tableHeaderRow = document.createElement("li");
+  const tableHeaderRow = document.createElement('li');
+
+  tableHeaderRow.onclick = (e) => {
+    this.sortByTableHeaderRow(e);
+  };
+
   tableHeaderRow.classList.add(this.classes.row);
 
   this.headers.forEach((cell) => {
-    tableHeaderRow.innerHTML += `<span class="${this.classes.cell}">${cell.title}</span>`;
+    tableHeaderRow.innerHTML += `<span data-prop-name="${cell.name}" class="${this.classes.cell} ${
+      cell.sort ? this.classes.cellUnsorted : ''
+    }">${cell.title}</span>`;
   });
+
   tableHeader.append(tableHeaderRow);
 
-  this.tableBody = document.createElement("ul");
   this.tableBody.classList.add(this.classes.table);
+
+  this.renderTableBody();
 
   if (this.rowClick) {
     this.tableBody.onclick = (e) => {
       this.rowClickHandler(e);
     };
   }
-
-  this.collection.forEach((row, index) => this.createSingleRow(row, index));
   parent.append(tableHeader, this.tableBody);
 };
 
-UniversalTable.prototype.createSingleRow = function (row, index) {
-  const tableRow = document.createElement("li");
-  tableRow.classList.add(this.classes.row);
+UniversalTable.prototype.renderTableBody = function () {
+  this.tableBody.replaceChildren();
+  this.collection.forEach((row, index) => {
+    const tableRow = document.createElement('li');
+    tableRow.classList.add(this.classes.row);
 
-  tableRow.dataset.uniqueMarker = this.generateDataset?.(row) || index;
-  tableRow.dataset.collectionInd = index;
-  tableRow.classList.add("t-row");
+    tableRow.dataset.ind = index;
+    tableRow.classList.add(this.classes.row);
 
-  tableRow.innerHTML = this.headers
-    .map(
-      ({ name }) =>
-        `<span class="${this.classes.cell}">${
-          row[name] || this.emptyCellValue
-        }</span>`
-    )
-    .join("");
+    tableRow.innerHTML = this.headers
+      .map(
+        ({ name }) =>
+          `<span class="${this.classes.cell}">${row[name] || this.emptyCellValue}</span>`,
+      )
+      .join('');
 
-  this.tableBody.append(tableRow);
+    this.tableBody.append(tableRow);
+  });
+};
+
+UniversalTable.prototype.sortByTableHeaderRow = function (e) {
+  const targetElem = e.target;
+
+  if (e.target.tagName === 'SPAN') {
+    if (
+      !targetElem.classList.contains(this.classes.cellUnsorted) &&
+      !targetElem.classList.contains(this.classes.cellSorted)
+    ) {
+      return;
+    }
+
+    if (targetElem.classList.contains(this.classes.cellUnsorted)) {
+      targetElem.classList.remove(this.classes.cellUnsorted);
+      targetElem.classList.add(this.classes.cellSorted);
+      
+    } else if (targetElem.classList.contains(this.classes.cellSorted)) {
+      targetElem.classList.remove(this.classes.cellSorted);
+      targetElem.classList.add(this.classes.cellUnsorted);
+    }
+
+    const oldSortingHeader = this.headers.find(({ sortBy }) => sortBy);
+    const headerToSortWith = this.headers.find((el) => {
+      return el.name === targetElem.dataset.propName;
+    });
+
+    if (headerToSortWith.sort) {
+      this.collection = this.collection.sort(headerToSortWith.sort);      
+
+      //TODO: toggle the sortBy property in the old sorting header
+      //TODO:  toggle the sortBy property in the new sorting header
+
+      this.renderTableBody();
+    }
+  }
 };
 
 UniversalTable.prototype.rowClickHandler = function (e) {
   let clickedIndex = 0;
-  const somePropertyName = this.headers[0].name;
-  this.collection.find((item) => item[somePropertyName]);
+
   if (!e.target.classList.contains(this.classes.row)) {
-    clickedIndex = e.target.closest(`.${this.classes.row}`).dataset
-      .collectionInd;
+    clickedIndex = e.target.closest(`.${this.classes.row}`).dataset;
   } else {
-    clickedIndex = e.target.dataset.collectionInd;
+    clickedIndex = e.target.dataset;
   }
   this.rowClick(this.collection[clickedIndex]);
-};
-
-UniversalTable.prototype.updateTable = function (updatedCollection) {
-  this.tableBody.replaceChildren();
-  updatedCollection.forEach((row, index) => this.createSingleRow(row, index));
-
-  this.collection = updatedCollection;
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,67 +1,78 @@
-import { getDoc } from "firebase/firestore";
+import { getDoc } from 'firebase/firestore';
 
-import { getWallets, getTransactions } from "@/API";
-import { UniversalTable, TransactionForm, ModalWindow } from "@/components";
+import { getWallets, getTransactions } from '@/API';
+import { UniversalTable, TransactionForm, ModalWindow } from '@/components';
 
-import "./Home.scss";
+import './Home.scss';
 
 export default function Home() {
   this.modal = new ModalWindow();
-  this.newTransactionButton = document.createElement("button");
-  this.pageWrapper = document.createElement("div");
-  this.balanceWrapper = document.createElement("section");
-  this.balanceText = document.createElement("h2");
-  this.totalBalance = document.createElement("h2");
-  this.currency = document.createElement("h2");
+  this.newTransactionButton = document.createElement('button');
+  this.pageWrapper = document.createElement('div');
+  this.balanceWrapper = document.createElement('section');
+  this.balanceText = document.createElement('h2');
+  this.totalBalance = document.createElement('h2');
+  this.currency = document.createElement('h2');
 
-  this.walletsWrapper = document.createElement("section");
-  this.walletsHeader = document.createElement("h2");
+  this.walletsWrapper = document.createElement('section');
+  this.walletsHeader = document.createElement('h2');
 
-  this.transactionsWrapper = document.createElement("section");
-  this.transactionsHeader = document.createElement("h2");
+  this.transactionsWrapper = document.createElement('section');
+  this.transactionsHeader = document.createElement('h2');
   this.transactionsTable = null;
 }
 
 Home.prototype.render = async function (parent) {
-  this.balanceWrapper.classList.add("balance--wrapper");
-  this.balanceText.textContent = "Total balance";
-  this.balanceText.classList.add("balance--header");
+  this.balanceWrapper.classList.add('balance--wrapper');
+  this.balanceText.textContent = 'Total balance';
+  this.balanceText.classList.add('balance--header');
   this.totalBalance.textContent = 0;
-  this.totalBalance.classList.add("balance--count");
-  this.currency.textContent = "$";
-  this.currency.classList.add("balance--count");
+  this.totalBalance.classList.add('balance--count');
+  this.currency.textContent = '$';
+  this.currency.classList.add('balance--count');
 
-  this.walletsWrapper.classList.add("wallets--wrapper");
-  this.walletsHeader.textContent = "Your wallets";
+  this.walletsWrapper.classList.add('wallets--wrapper');
+  this.walletsHeader.textContent = 'Your wallets';
 
-  this.pageWrapper.classList.add("page-wrapper");
-  this.transactionsWrapper.classList.add("transactions--wrapper");
-  this.transactionsHeader.classList.add("transactions--header");
-  this.transactionsHeader.textContent = "Transactions";
+  this.pageWrapper.classList.add('page-wrapper');
+  this.transactionsWrapper.classList.add('transactions--wrapper');
+  this.transactionsHeader.classList.add('transactions--header');
+  this.transactionsHeader.textContent = 'Transactions';
 
-  this.newTransactionButton.innerText = "New transaction";
-  this.newTransactionButton.classList.add("new-transaction-btn");
+  this.newTransactionButton.innerText = 'New transaction';
+  this.newTransactionButton.classList.add('new-transaction-btn');
   this.newTransactionButton.onclick = (event) => this.handleCreateForm(event);
 
   const wallets = await getWallets();
 
-  const totalBalance = wallets.reduce(
-    (acc, currWallet) => (acc += +currWallet.balance),
-    0
-  );
+  const totalBalance = wallets.reduce((acc, currWallet) => (acc += +currWallet.balance), 0);
   this.totalBalance.textContent = totalBalance;
 
   const transactions = await this.pullAllTransaction();
 
   this.transactionsTable = new UniversalTable(transactions, {
     headers: [
-      { name: "category", title: "Category" },
-      { name: "amount", title: "Amount" },
-      { name: "from", title: "From" },
-      { name: "to", title: "To" },
-      { name: "comment", title: "Comment" },
-      { name: "date", title: "Date" },
-      { name: "type", title: "Type" },
+      {
+        name: 'category',
+        title: 'Category',
+        sortBy: false,
+      },
+      {
+        name: 'amount',
+        title: 'Amount',
+        sortBy: false,
+        sort: (a, b) => Number(b.amount) - Number(a.amount),
+      },
+      { name: 'from', title: 'From', sortBy: false },
+      { name: 'to', title: 'To', sortBy: false },
+      { name: 'comment', title: 'Comment', sortBy: false },
+      {
+        name: 'date',
+        title: 'Date',
+        sortBy: true,
+        sort: (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(),
+      },
+      { name: 'type', title: 'Type', sortBy: false },
     ],
   });
 
@@ -71,16 +82,17 @@ Home.prototype.render = async function (parent) {
 
   this.walletsTable = new UniversalTable(wallets, {
     headers: [
-      { name: "name", title: "Name" },
-      { name: "balance", title: "Balance" },
+      {
+        name: 'name',
+        title: 'Name',
+        sortBy: true,
+        sort: (a, b) => a.date - b.date,
+      },
+      { name: 'balance', title: 'Balance', sortBy: false, sort: (a, b) => a.date - b.date },
     ],
   });
 
-  this.balanceWrapper.append(
-    this.balanceText,
-    this.currency,
-    this.totalBalance
-  );
+  this.balanceWrapper.append(this.balanceText, this.currency, this.totalBalance);
   this.walletsWrapper.replaceChildren();
   this.walletsWrapper.append(this.walletsHeader);
   this.walletsTable.render(this.walletsWrapper);
@@ -88,7 +100,7 @@ Home.prototype.render = async function (parent) {
     this.balanceWrapper,
     this.walletsWrapper,
     this.newTransactionButton,
-    this.transactionsWrapper
+    this.transactionsWrapper,
   );
   parent.append(this.pageWrapper);
 };
@@ -103,8 +115,8 @@ Home.prototype.handleCreateForm = function (event) {
     },
   });
 
-  this.modal.render(document.getElementById("app"), newTransactionForm);
-  console.log("new transaction");
+  this.modal.render(document.getElementById('app'), newTransactionForm);
+  console.log('new transaction');
 };
 
 Home.prototype.pullAllTransaction = async function () {
@@ -118,8 +130,8 @@ Home.prototype.pullAllTransaction = async function () {
         from: t.from ? (await getDoc(t.from)).data().name : null,
         category: t.category ? (await getDoc(t.category)).data().name : null,
         date: t.date.toDate().toLocaleString(),
-        comment: !t.comment ? "Empty" : t.comment,
+        comment: !t.comment ? 'Empty' : t.comment,
       };
-    })
+    }),
   );
 };
