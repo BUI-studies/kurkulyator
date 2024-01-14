0d90296b200e9701358789dc570cc3d1ad8a9aaf
Roman Ohiienko <ohiienko.r@gmail.com>
2024-01-14 12:38:07 +0200

    update transactions filtration method

--- src/pages/TransactionsHistory/TransactionsHistory.js
+++ src/pages/TransactionsHistory/TransactionsHistory.js
@@ -1,8 +1,10 @@
-import { getTransactions, getWallets, getTransactionsByWallet } from '@/API'
+import { getTransactions, getWallets, getTransactionsByWallet, getCategories } from '@/API'
 import { createElement, createSelect, makeOptions } from '@/utils'
-
 import { UniversalButton, UniversalTable } from '@/components'
-
+import { TRANSACTION_TYPE } from '@/types'
+import { transactionsCollectionRef, walletsCollectionRef } from '../../../firebase'
+import { doc, query, and, where, or, getDocs, getDoc } from 'firebase/firestore'
+import { Router } from '@/routes'
 import './_TransactionsHistory.scss'
 
 export default function TransactionsHistory() {
@@ -15,8 +17,9 @@ export default function TransactionsHistory() {
     className: 'transactions__title',
     innerText: 'Transactions:',
   })
-  this.filtersWrapper = createElement({
-    tagName: 'section',
+  this.filtersForm = createElement({
+    tagName: 'form',
+    name: 'filters-form',
     className: 'filters',
   })
   this.filtersHeader = createElement({
@@ -25,7 +28,7 @@ export default function TransactionsHistory() {
     innerText: 'Filters:',
   })
   this.filterByWalletLabel = createElement({
-    tagName: 'Label',
+    tagName: 'label',
     name: 'filter-bw-label',
     id: 'fbwLabel',
     innerText: 'Wallets:',
@@ -35,10 +38,21 @@ export default function TransactionsHistory() {
     name: 'filter-by-wallet',
     className: 'filters__filter-by-wallet',
   })
+  this.filterByTransactionTypeLabel = createElement({
+    tagName: 'label',
+    name: 'filter-bt-label',
+    id: 'fbtLabel',
+    innerText: 'Type:',
+    className: 'filters__label',
+  })
+  this.filterByTransactionType = createSelect({
+    name: 'filter-by-type',
+    className: 'filters__filter-by-type',
+  })
   this.filterButton = new UniversalButton({
     text: 'Filter',
     className: 'filters__filter-btn',
-    onClick: (e) => this.handleFilterByWallet(e),
+    onClick: (e) => this.handleFilter(e),
   })
   this.resetButton = new UniversalButton({
     text: 'Reset',
@@ -51,14 +65,21 @@ export default function TransactionsHistory() {
 TransactionsHistory.prototype.render = async function (parent) {
   this.filterByWallet.innerHTML = []
   const wallets = await getWallets()
-  const walletsOptions = makeOptions(wallets, 'filter_wallets-options')
+  const walletsOptions = makeOptions(wallets, 'filters__wallets-options')
   this.filterByWallet.innerHTML = walletsOptions
 
   this.filterByWalletLabel.append(this.filterByWallet)
 
-  this.filtersWrapper.append(this.filtersHeader, this.filterByWalletLabel)
-  this.filterButton.render(this.filtersWrapper)
-  this.resetButton.render(this.filtersWrapper)
+  this.filterByTransactionType.innerHTML = []
+  const types = [...Object.values(TRANSACTION_TYPE)]
+  const typesOptions = makeOptions(types, 'filters__type-options')
+  this.filterByTransactionType.innerHTML = typesOptions
+
+  this.filterByTransactionTypeLabel.append(this.filterByTransactionType)
+
+  this.filtersForm.append(this.filtersHeader, this.filterByWalletLabel, this.filterByTransactionTypeLabel)
+  this.filterButton.render(this.filtersForm)
+  this.resetButton.render(this.filtersForm)
 
   const transactions = await getTransactions()
 
@@ -94,17 +115,64 @@ TransactionsHistory.prototype.render = async function (parent) {
   this.transactionsWrapper.append(this.transactionsHeader)
   this.transactionsTable.render(this.transactionsWrapper)
 
-  parent.append(this.filtersWrapper, this.transactionsWrapper)
+  parent.append(this.filtersForm, this.transactionsWrapper)
 }
-
-TransactionsHistory.prototype.handleFilterByWallet = async function (e) {
+//TODO: refactor
+TransactionsHistory.prototype.handleFilter = async function (e) {
   e.preventDefault()
-  const transactionsByWallet = await getTransactionsByWallet(this.filterByWallet.value)
-  this.transactionsTable.updateTable(transactionsByWallet)
-}
+  const query = this.buildQuery()
+  const responseSnapShot = await getDocs(query)
+  const res = []
+  responseSnapShot.forEach((p) => res.push({ id: p.id, ...p.data() }))
+  const upd = await Promise.all(
+    res.map(async (t) => {
+      return {
+        ...t,
+        to: t.to ? (await getDoc(t.to)).data().name : null,
+        from: t.from ? (await getDoc(t.from)).data().name : null,
+        category: t.category ? (await getDoc(t.category)).data().name : null,
+        date: t.date.toDate().toLocaleString(),
+        comment: !t.comment ? 'Empty' : t.comment,
+        delete: `<button id="${t.id}" class="remove-transaction"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg></button>`,
+      }
+    })
+  )
 
+  this.transactionsTable.updateTable(upd)
+}
+//TODO: refactor
 TransactionsHistory.prototype.handleResetFilters = async function (e) {
   e.preventDefault()
   const transactions = await getTransactions()
   this.transactionsTable.updateTable(transactions)
+  this.filtersForm.reset()
+}
+
+TransactionsHistory.prototype.buildQuery = function () {
+  let getQuery = null
+  const fData = new FormData(this.filtersForm)
+
+  const data = {
+    wallet: fData.get('filter-by-wallet'),
+    type: fData.get('filter-by-type'),
+  }
+
+  const getWalletWhere = () => {
+    if (!data.wallet) return null
+    const walletRef = doc(walletsCollectionRef, data.wallet)
+    const walletQry = or(where('to', '==', walletRef), where('from', '==', walletRef))
+    return walletQry
+  }
+
+  const getTypeWhere = () => {
+    if (!data.type) return null
+    const typeRef = where('type', '==', data.type)
+    return typeRef
+  }
+
+  getQuery = query(
+    transactionsCollectionRef,
+    and(where('owner', '==', Router.getCurrentUser().uid), and(...[getWalletWhere(), getTypeWhere()].filter(Boolean)))
+  )
+  return getQuery
 }
